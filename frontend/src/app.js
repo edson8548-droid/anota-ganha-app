8bafe3ab93d566925b9cb5abb2af5b53d0da6910
edson8548-droid <edson854_8@hotmail.com>
2025-10-27 20:23:27 -0300

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import './app.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { PrivateRoute } from "./components/PrivateRoute";
