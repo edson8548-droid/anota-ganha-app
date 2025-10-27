e5ef80fa046e109bf28f94a628c5d72a6fb9ffa9
edson8548-droid <edson854_8@hotmail.com>
2025-10-27 18:21:35 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
-import app from './App';
+import app from './app';
 import { Toaster } from "sonner";
 
 // Registrar Service Worker para PWA
